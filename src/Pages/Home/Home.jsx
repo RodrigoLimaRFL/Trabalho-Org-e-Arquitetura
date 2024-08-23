d2940dd2ff3710ed3c5a2dea19d4f6802016889c
Rodrigo Lima <guico21@gmail.com>
2024-08-23 18:02:50 -0300

    a

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,6 +13,11 @@ const Home = () => {
       <Header />
       <div className={'description'}> 
         <h1> RISC-V e a sua Cache </h1>
+        <p> Este site foi criado com o objetivo de fornecer um estudo detalhado sobre o funcionamento e a importância da memória cache
+           na arquitetura de computadores. Nele, exploramos conceitos fundamentais, mecanismos de operação e práticas relacionadas ao cache,
+            como mapeamentos, algoritmos de substituição e políticas de escrita. Destinado a qualquer um que esteja interessado no assunto,
+             o conteúdo aqui apresentado busca esclarecer, com profundidade e clareza, 
+          os principais aspectos desse elemento crucial para a Organização e Arquitetura de Computadores.</p>
       </div>
       <div className={'button-container'}>
         <Button className={'home-button'} onClick={() => navigate('/Trabalho-Org-e-Arquitetura/o-que-e')}>
